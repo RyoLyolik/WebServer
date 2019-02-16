b05b2bcf19225a549e963390586b7d68465d949e
bicho <no>
2019-02-16 20:48:39 +0300

    Something done

--- layout.py
+++ layout.py
@@ -1,13 +1,30 @@
 from flask import *
 from flask_wtf import *
 from wtforms import *
+from lorem import *
 
 app = Flask(__name__)
+class Pages:
+    def __init__(self):
+        pass
 
+    def about(self):
+        return render_template('about.html', title='About', lorem=what)
+
+    def home(self):
+        return render_template('home.html', title='Home', info=info, to_do=to_do)
+pages = Pages()
+@app.route('/about')
+def about():
+    return pages.about()
+
+@app.route('/home')
+def home():
+    return pages.home()
 
 @app.route('/')
-def index():
-    return render_template('index.html', title='About', header='Home')
+def ret():
+    return redirect('/home')
 
 
 if __name__ == '__main__':
